745f5b9bf176097984194176c8d75b271fa7decf
Amelia Holloway <57925045+AkinaMisaki@users.noreply.github.com>
2025-05-31 19:18:56 -0300

    Limpa do código

--- Autenticador1.py
+++ Autenticador1.py
@@ -1,44 +1,69 @@
 import hashlib
+import os
 
 nome = 0
 senha = 0
 senha_hash = 0
+nome = ''
+senha = ''
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+passwd_dir = os.path.join(__location__, 'usuarios.txt')
+
+def input_usuario():
+    global nome, senha
+    while len(nome) != 4 or len(senha) != 4:
+            
+            nome = input("Digite o nome do usuario: ")
+
+            if len(nome) != 4:
+                print("Nome deve ter 4 caracteres.")
+                continue
+
+            senha = input("Digite a senha do usuario: ")
+
+            if len(senha) != 4:
+                print("Senha deve ter 4 caracteres.")
+                continue
 
 def cadastrar_usuario(nome, senha):
-    if len(nome) != 4 or len(senha) != 4:
-        print("Nome e senha devem ter 4 caracteres.")
-        return
+
     senha_hash = hashlib.md5(senha.encode()).hexdigest()
-    with open('usuarios.txt', 'a') as f:
+
+    with open(passwd_dir, 'a') as f:
         f.write(f"{nome},{senha_hash}\n")
+
     print("Usuario cadastrado com sucesso.")
 
 def autenticar_usuario(nome, senha):
+
     try:
-        with open('usuarios.txt', 'r') as f:
+        with open(passwd_dir, 'r') as f:
             for x in f:
                 n, s = x.strip().split(',')
                 if n == nome and hashlib.md5(senha.encode()).hexdigest() == s:
                     print("Autenticaçao bem-sucedida.")
                     return
             print("Nome ou senha incorretos.")
+            
     except FileNotFoundError:
         print("Arquivo de usuarios não encontrado.")
 
 while True:
-    print("1. Cadastrar usuario")
-    print("2. Autenticar usuario")
-    print("3. Sair")
+    print("1. Cadastrar usuarioz\n2. Autenticar usuario\n3. Sair")
     opcao = input("Escolha uma opção: ")
     if opcao == '1':
-        nome = input("Digite o nome do usuario: ")
-        senha = input("Digite a senha do usuario: ")
+
+        input_usuario()
         cadastrar_usuario(nome, senha)
+        
     elif opcao == '2':
-        nome = input("Digite o nome do usuario: ")
-        senha = input("Digite a senha do usuario: ")
+
+        input_usuario()
         autenticar_usuario(nome, senha)
+
     elif opcao == '3':
-        break
+        exit()
+
     else:
         print("Opçao invalida.")
